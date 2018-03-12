891e567c86ecb1a8999528e591942360e8123ed2
Polskig <kimjankowski@outlook.com>
2018-03-12 13:56:17 +0100

    Vibration

---
+++ Vibration/test.js
@@ -0,0 +1,29 @@
+/*function example1() {
+   Vibrate for 500ms
+  navigator.vibrate([500]);
+}*/
+
+function example2() {
+    // For a single value you can pass in a Number rather than an Array
+    navigator.vibrate(500);
+  }
+  
+  function startPattern() {
+    // Values at even indices (0, 2, 4, ...) specify vibrations, while the odd
+    // indices specify pauses.
+    // Vibrate for 500ms 6 times, pausing for 250ms in between each one.
+    navigator.vibrate([400, 250, 400, 250, 400, 250, 400]);
+  }
+  
+  function startPattern2() {
+    // Values at even indices (0, 2, 4, ...) specify vibrations, while the odd
+    // indices specify pauses.
+    // Vibrate for 500ms 6 times, pausing for 250ms in between each one.
+    navigator.vibrate([250, 400, 250, 400, 250, 400, 250]);
+  }
+  
+  function stopVibrations() {
+    // You can also stop an ongoing vibration pattern by specifying a vibration
+    // length of zero.
+    navigator.vibrate(0);
+  }
