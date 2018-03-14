ea0f5f39a520483880023b22c23d68e4c0025de8
Polskig <kimjankowski@outlook.com>
2018-03-14 14:14:24 +0100

    vibration

--- Vibration/test.js
+++ Vibration/test.js
@@ -6,14 +6,14 @@ function example2() {
   function startPattern() {
     // Values at even indices (0, 2, 4, ...) specify vibrations, while the odd
     // indices specify pauses.
-    // Vibrate for 500ms 6 times, pausing for 250ms in between each one.
+    // Vibrate for 400ms 6 times, pausing for 250ms in between each one.
     navigator.vibrate([400, 250, 400, 250, 400, 250, 400]);
   }
   
   function startPattern2() {
     // Values at even indices (0, 2, 4, ...) specify vibrations, while the odd
     // indices specify pauses.
-    // Vibrate for 500ms 6 times, pausing for 250ms in between each one.
+    // Vibrate for 250ms 6 times, pausing for 400ms in between each one.
     navigator.vibrate([250, 400, 250, 400, 250, 400, 250]);
   }
   
