3bfa521463ea61a019c55ceb8277e1e522fa31ff
Polskig <kimjankowski@outlook.com>
2018-03-16 11:55:14 +0100

    Test

--- Consepts/Vibrations/Vibration3/test.js
+++ Consepts/Vibrations/Vibration3/test.js
@@ -1,28 +1,32 @@
-function myFunction() {
-  alert('Hello');
+function myFunction1() {
+  document.getElementById("pattern1").innerHTML = "Available";
 }
 
-function example() {
-    // For a single value you can pass in a Number rather than an Array
-    navigator.vibrate(500);
-  }
+function myFunction2() {
+  document.getElementById("pattern2").innerHTML = "Booked/Unavailable";
+}
+
+function myFunction3() {
+  document.getElementById("pattern3").innerHTML = "You going to be kicked out";
+}
   
-  function startPattern() {
-    // Values at even indices (0, 2, 4, ...) specify vibrations, while the odd
-    // indices specify pauses.
-    // Vibrate for 400ms 6 times, pausing for 250ms in between each one.
-    navigator.vibrate([400, 250, 400, 250, 400, 250, 400]);
-  }
-  
-  function startPattern2() {
-    // Values at even indices (0, 2, 4, ...) specify vibrations, while the odd
-    // indices specify pauses.
-    // Vibrate for 250ms 6 times, pausing for 400ms in between each one.
-    navigator.vibrate([250, 400, 250, 400, 250, 400, 250]);
-  }
-  
-  function stopVibrations() {
-    // You can also stop an ongoing vibration pattern by specifying a vibration
-    // length of zero.
-    navigator.vibrate(0);
-  }
+function startPattern() {
+  // Values at even indices (0, 2, 4, ...) specify vibrations, while the odd
+  // indices specify pauses.
+  // Vibrate for 400ms 6 times, pausing for 250ms in between each one.
+  navigator.vibrate([400, 250, 400, 250, 400]);
+}
+
+function startPattern2() {
+  // Values at even indices (0, 2, 4, ...) specify vibrations, while the odd
+  // indices specify pauses.
+  // Vibrate for 250ms 6 times, pausing for 400ms in between each one.
+  navigator.vibrate([250, 400, 250, 400, 250]);
+}
+
+function startPattern3() {
+  // Values at even indices (0, 2, 4, ...) specify vibrations, while the odd
+  // indices specify pauses.
+  // Vibrate for 250ms 6 times, pausing for 400ms in between each one.
+  navigator.vibrate([750, 50, 750, 50, 750, 50, 750]);
+}
