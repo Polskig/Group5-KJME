182395cf70f0bbdfa84c4e3e0c9b5a9e6a9071f6
Polskig <kimjankowski@outlook.com>
2018-03-16 11:34:22 +0100

    Test

--- Consepts/Vibrations/Vibration2/test.js
+++ Consepts/Vibrations/Vibration2/test.js
@@ -1,20 +1,20 @@
-  function startPattern() {
-    // Values at even indices (0, 2, 4, ...) specify vibrations, while the odd
-    // indices specify pauses.
-    // Vibrate for 400ms 6 times, pausing for 250ms in between each one.
-    navigator.vibrate([400, 250, 400, 250, 400, 250, 400]);
-  }
-  
-  function startPattern2() {
-    // Values at even indices (0, 2, 4, ...) specify vibrations, while the odd
-    // indices specify pauses.
-    // Vibrate for 250ms 6 times, pausing for 400ms in between each one.
-    navigator.vibrate([250, 400, 250, 400, 250, 400, 250]);
-  }
+function startPattern() {
+  // Values at even indices (0, 2, 4, ...) specify vibrations, while the odd
+  // indices specify pauses.
+  // Vibrate for 400ms 6 times, pausing for 250ms in between each one.
+  navigator.vibrate([400, 250, 400, 250, 400]);
+}
 
-  function startPattern3() {
-    // Values at even indices (0, 2, 4, ...) specify vibrations, while the odd
-    // indices specify pauses.
-    // Vibrate for 250ms 6 times, pausing for 400ms in between each one.
-    navigator.vibrate([250, 400, 250, 400, 250, 400, 250]);
-  }
+function startPattern2() {
+  // Values at even indices (0, 2, 4, ...) specify vibrations, while the odd
+  // indices specify pauses.
+  // Vibrate for 250ms 6 times, pausing for 400ms in between each one.
+  navigator.vibrate([250, 400, 250, 400, 250]);
+}
+
+function startPattern3() {
+  // Values at even indices (0, 2, 4, ...) specify vibrations, while the odd
+  // indices specify pauses.
+  // Vibrate for 250ms 6 times, pausing for 400ms in between each one.
+  navigator.vibrate([750, 50, 750, 50, 750, 50, 750]);
+}
