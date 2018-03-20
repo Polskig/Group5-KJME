3d3cbbc654f6dbee9ed8c20afa61dcc751f85be2
Polskig <kimjankowski@outlook.com>
2018-03-20 13:52:24 +0100

    Vibrations
    Makes so that the phone vibrates

--- Vibration/Vibration Cross-device/public/script.js
+++ Vibration/Vibration Cross-device/public/script.js
@@ -8,34 +8,57 @@ function ready() {
   const url = 'ws://' + location.host + '/ws';
   socket = new ReconnectingWebsocket(url);
 
-  socket.onopen = function(evt) {
-    console.log('Web socket opened: ' + url);
-  };
-
+  //get the square to dispaly on screen
   socket.onmessage = function(evt) {
     logReceived(evt.data);
     if (evt.data == 'Pattern1') {
-      document.getElementById("ruta").style.display = "flex";
+      document.getElementById("ruta1").style.display = "flex";
+    }
+    if (evt.data == 'Pattern2') {
+      document.getElementById("ruta2").style.display = "flex";
+    }
+    if (evt.data == 'Pattern3') {
+      document.getElementById("ruta3").style.display = "flex";
     }
   };
 
+  //Get the elements from startPattern1
   document.getElementById("startPattern1").addEventListener('click', function() {
     send("Pattern1");
     console.log ("hello1");
   });
 
-
-  document.getElementById("ruta").addEventListener('click', function() {
-    document.getElementById("ruta").style.display = "none";
+  //adds so that when you press the square you get the vibration to work on the phone
+  document.getElementById("ruta1").addEventListener('click', function() {
+    document.getElementById("ruta1").style.display = "none";
     navigator.vibrate([400, 250, 400, 250, 400]);
   });
 
-  document.getElementById("startPattern2").addEventListener('click', function(evt) {
+
+
+  //Get the elements from startPattern2
+  document.getElementById("startPattern2").addEventListener('click', function() {
     send("Pattern2");
+    console.log ("hello2");
   });
 
-  document.getElementById("startPattern3").addEventListener('click', function(evt) {
-    send("Pattern1");
+  //adds so that when you press the square you get the vibration to work on the phone
+  document.getElementById("ruta2").addEventListener('click', function() {
+    document.getElementById("ruta2").style.display = "none";
+    navigator.vibrate([250, 400, 250, 400, 250]);
+  });
+
+
+
+  document.getElementById("startPattern3").addEventListener('click', function() {
+    send("Pattern3");
+    console.log ("hello");
+  });
+
+  //adds so that when you press the square you get the vibration to work on the phone
+  document.getElementById("ruta3").addEventListener('click', function() {
+    document.getElementById("ruta3").style.display = "none";
+    navigator.vibrate([750, 50, 750, 50, 750, 50, 750]);
   });
 
 }
