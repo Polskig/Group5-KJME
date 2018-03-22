ba42d16b799825cb2c2d9714402c2f9ab3f9ca5b
marjanadragicevic <marjanadragicevic1@gmail.com>
2018-03-22 18:43:19 +0100

    Testing websocket

--- door-light/app.js
+++ door-light/app.js
@@ -4,6 +4,7 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var expressWs = require('express-ws');
+var five = require("johnny-five");
 
 var ews = expressWs(express());
 var app = ews.app;
@@ -14,6 +15,75 @@ var app = ews.app;
 app.ws('/ws', function (ws, req) {
   // A message has been received from a client
   ws.on('message', function (msg) {
+    var clients = ews.getWss('/ws').clients;
+    // Debug print it
+
+    console.log(new Date().toLocaleTimeString() + '> ' + msg);
+
+    // Broadcast it to all other clients
+    clients.forEach(c => {
+      c.send(msg);
+    });
+  });
+});
+
+
+//A series of functions that will turn a led on and off from the browser
+
+
+var five = require("johnny-five");
+var board = new five.Board();
+
+board.on("ready", function() {
+  // Set up the following PWM pins as LEDs.
+  // Fade an LED out, and the complete callback will start
+  // fading the next LED in sequence out, and so on.
+  // If randomFade is true, then fading will happen in random
+  // order instead of sequentially.
+  var leds = new five.Leds([11, 10, 9, 6, 5, 3]);
+  var timing = 1000;
+  var randomFade = false;
+  var fadeIndex = 0;
+  var ledCount = leds.length;
+  var i;
+
+  function fadeNext() {
+    var candidateIndex = fadeIndex;
+    leds[fadeIndex].fadeIn(timing);
+
+    // Determine the next LED to fade
+    if (randomFade) {
+      while (candidateIndex === fadeIndex) {
+        candidateIndex = Math.round(Math.random() * (ledCount - 1));
+      }
+    } else {
+      candidateIndex = (fadeIndex < ledCount - 1) ? fadeIndex + 1 : 0;
+    }
+    fadeIndex = candidateIndex;
+
+    leds[fadeIndex].fadeOut(timing, fadeNext);
+  }
+
+});
+
+
+  //Starts the robot when live server is started
+
+
+
+//recieves a message from the browser and if text matches, triggers a function in the robot
+app.ws('/ws', function (ws, req) {
+  ws.on('message', function (msg) {
+  
+
+	
+// A series of if statements waiting for browser to make a request
+  if(msg =="lamp"){
+    console.log("heha");
+  board.fadeNext();
+  }	
+    
+
     var clients = ews.getWss('/ws').clients;
     // Debug print it
 
